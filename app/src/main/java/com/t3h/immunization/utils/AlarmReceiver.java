bc35c2a615cef99f6bf3e2657bb10f3cbd8b04ff
Admin <trinhxuantien36@gmail.com>
2020-02-07 16:34:35 +0700

    gggg

---
+++ app/src/main/java/com/t3h/immunization/utils/AlarmReceiver.java
@@ -0,0 +1,41 @@
+package com.t3h.immunization.utils;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.TaskStackBuilder;
+
+import com.t3h.immunization.R;
+import com.t3h.immunization.activity.MainActivity;
+import com.t3h.immunization.activity.NotificationActivity;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Calendar now = GregorianCalendar.getInstance();
+        int dayOfWeek = now.get(Calendar.DATE);
+        if(dayOfWeek != 1 && dayOfWeek != 7) {
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(context)
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setContentTitle(context.getResources().getString(R.string.message_box_title))
+                            .setContentText("");
+            Intent resultIntent = new Intent(context, NotificationActivity.class);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            stackBuilder.addParentStack(MainActivity.class);
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.notify(1, mBuilder.build());
+        }
+    }
+    }
+
